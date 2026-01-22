8e07dd4fedb60c82eb3b1160bcd2bedf5050ceb0
chanwon05 <165672373+chanwon05@users.noreply.github.com>
2026-01-22 23:39:32 +0900

    Clarify sorting step with a comment
    Added comment to clarify sorting step

--- 순열.cpp
+++ 순열.cpp
@@ -7,7 +7,7 @@ int main()
     cin.tie(0);
     ios_base::sync_with_stdio(0);
     vector<int> a = {2, 1, 3};
-    sort(a.begin(), a.end());
+    sort(a.begin(), a.end());             // 먼저 오름차순 정렬
     do{
         for(int i : a) cout << i << " ";
         cout << '\n';
