b741b5aa60db81912019e9e1f90df1b0b5dbc4d0
chanwon05 <165672373+chanwon05@users.noreply.github.com>
2026-02-04 22:21:31 +0900

    Create prefix_sum.cpp

---
+++ prefix_sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[10004], b, c, psum[10004], n, m;
+
+int main()
+{   
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+        psum[i] = psum[i-1] + a[i];
+    }
+    for(int i = 0; i < m; i++){
+        cin >> b >> c;
+        cout << psum[c] - psum[b-1] << '\n';
+    }
+    
+    return 0;
+}
